0db3bb3d502e989bbf48d265f9829bef01b82a15
Paul Trilsbeek <Paul.Trilsbeek@mpi.nl>
2017-11-08 11:38:04 +0100

    simple search box tweaks

--- bootstrap_tla/template.php
+++ bootstrap_tla/template.php
@@ -1,5 +1,29 @@
 <?php
-  $theme['imce_root_text'] = array(
-    'variables' => array('hello' => NULL),
+
+/**
+ * Implements hook_form_alter().
+ */
+function bootstrap_tla_form_islandora_solr_simple_search_form_alter(&$form, &$form_state, $form_id) {
+  $form['simple'] = array(
+    '#type' => 'container',
+    '#attributes' => array(
+      'class' => array(
+        'container-inline',
+      ),
+    ),
   );
+  $form['simple']["islandora_simple_search_query"] = array(
+    '#size' => '15',
+    '#type' => 'textfield',
+    '#title' => t(""),
+    '#attributes' => array('placeholder' => t('Search'),
+        ),
+  );
+  $form['simple']['submit'] = array(
+    '#type' => 'submit',
+    '#value' => t('<span aria-hidden="true" class="icon glyphicon glyphicon-search"></span>'),
+  );
+  return $form;
+}
+
 ?>
